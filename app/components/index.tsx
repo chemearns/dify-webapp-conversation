e7086d9c643c938c350ce17f7806d0d5b97586d8
chemearns <che@jiminy.co.nz>
2025-07-01 20:46:28 +1200

    Reverse previous

--- app/components/index.tsx
+++ app/components/index.tsx
@@ -19,7 +19,7 @@ import useBreakpoints, { MediaType } from '@/hooks/use-breakpoints'
 import Loading from '@/app/components/base/loading'
 import { replaceVarWithValues, userInputsFormToPromptVariables } from '@/utils/prompt'
 import AppUnavailable from '@/app/components/app-unavailable'
-import { API_KEY, APP_ID, APP_INFO, SKIP_LANDING_SCREEN, isShowPrompt, promptTemplate } from '@/config'
+import { API_KEY, APP_ID, APP_INFO, isShowPrompt, promptTemplate } from '@/config'
 import type { Annotation as AnnotationType } from '@/types/log'
 import { addFileInfos, sortAgentSorts } from '@/utils/tools'
 import { error, log } from '@/utils/iframe-diagnostics'
@@ -272,7 +272,7 @@ const Main: FC<IMainProps> = () => {
 
         setInited(true)
 
-        if (SKIP_LANDING_SCREEN && isNewConversation) {
+        /* if (SKIP_LANDING_SCREEN && isNewConversation) {
           const prompt_variables = userInputsFormToPromptVariables(user_input_form)
           const defaultInputs: Record<string, any> = {}
           prompt_variables.forEach((variable) => {
@@ -284,7 +284,7 @@ const Main: FC<IMainProps> = () => {
             setChatStarted()
             setChatList(generateNewChatListWithOpenStatement('', defaultInputs))
           }, 100)
-        }
+        } */
       }
       catch (e: any) {
         if (e.status === 404) {
